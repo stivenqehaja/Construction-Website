63ac3422024d408d8187dd5ee1bd2a76771871a8
Stiven Qehaja <stivenqehaja@gmial.com>
2024-03-15 21:28:02 +0100

    [REFACTOR]: Home Slider & Footer

--- app.py
+++ app.py
@@ -7,6 +7,25 @@ app = Flask(__name__)
 def index():
     return render_template('main-page.html')
 
+# *_______________NAVBAR ROUTES_______________*
+@app.route('/about')
+def about():
+    return render_template("nav-links/nav-about.html")
+@app.route('/services')
+def services():
+    return render_template("nav-links/nav-services.html")
+@app.route('/projects')
+def projects():
+    return render_template("nav-links/nav-projects.html")
+@app.route('/contact')
+def contact():
+    return render_template("nav-links/nav-contact.html")
+@app.route('/blogs')
+def blogs():
+    return render_template("nav-links/nav-blogs.html")
+
+
+
 # *_______________ERRORS_______________*
 # Invalid URL
 @app.errorhandler(404)
--- static/js/main-page-script.js
+++ static/js/main-page-script.js
@@ -38,66 +38,20 @@ document.addEventListener('DOMContentLoaded', function() {
    };
 });
 // Swiper Animation Functionality
-var swiper = new Swiper(".home-slider", {
-   loop: true,
-   grabCursor: true,
-   navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-   },
-});
-
-var swiper = new Swiper(".reviews-slider", {
-   loop: true,
-   grabCursor: true,
-   spaceBetween: 20,
-   breakpoints: {
-      640: {
-         slidesPerView: 1
-      },
-      768: {
-         slidesPerView: 2
-      },
-      991: {
-         slidesPerView: 3
-      },
-   }
-});
-
-var swiper = new Swiper(".blogs-slider", {
-   loop: true,
-   grabCursor: true,
-   spaceBetween: 20,
-   breakpoints: {
-      640: {
-         slidesPerView: 1
-      },
-      768: {
-         slidesPerView: 2
-      },
-      991: {
-         slidesPerView: 3
-      },
-   }
-});
-
-var swiper = new Swiper(".logo-slider", {
-   loop: true,
-   grabCursor: true, 
-   spaceBetween: 20,
-   breakpoints: {
-      450: {
-         slidesPerView: 2
-      },
-      640: {
-         slidesPerView: 3
-      },
-      768: {
-         slidesPerView: 4
-      },
-      1000: {
-         slidesPerView: 5
-      },
-   }
-});
-
+document.addEventListener('DOMContentLoaded', function() {
+   // Initialize Swiper
+   var swiper = new Swiper('.home-slider', {
+     // Optional parameters
+     direction: 'horizontal',
+     loop: true,
+     speed: 1000,
+     autoplay: {
+       delay: 5000,
+       disableOnInteraction: false,
+     },
+     navigation: {
+       nextEl: '.swiper-button-next',
+       prevEl: '.swiper-button-prev',
+     },
+   });
+ });
